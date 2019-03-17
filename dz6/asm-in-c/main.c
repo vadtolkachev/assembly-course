abfaf12615a97f1bce856a8c9024453ea113dd10
vadym <vadtolkachev@gmail.com>
2019-03-18 00:55:31 +0300

    added asm-in-c

---
+++ dz6/asm-in-c/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern void VPrintf(int a, int b, int c, int d, int e, int f, const char *str, ...);
+
+int main()
+{
+	VPrintf(0, 1, 2, 3, 4, 5, "hello world!\nI %s %x %d %% %c\n", "love", 3802, 100, 31);
+	
+	return 0;
+}
